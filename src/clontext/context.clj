a9ee216f701e2fa7f5c4c1cb20b677bdcb951027
Jamie <jfr@juxt.pro>
2024-09-19 20:58:37 +0100

    update indexing error

--- src/clontext/context.clj
+++ src/clontext/context.clj
@@ -8,13 +8,13 @@
   [project-dir output-file]
   (let [ignore-patterns (gitignore/read-gitignore project-dir)
         files (file/list-files project-dir)
+        filtered-files (remove #(gitignore/should-ignore? % project-dir ignore-patterns) files)
         context (str "<documents>\n"
                      (str/join "\n"
                        (map-indexed
                          (fn [idx file]
-                           (when-not (gitignore/should-ignore? file project-dir ignore-patterns)
-                             (file/file-content file (inc idx))))
-                         files))
+                           (file/file-content file (inc idx)))
+                         filtered-files))
                      "\n</documents>")]
     (spit output-file context)
     (println "Context file generated:" output-file)))
